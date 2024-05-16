24e39a769ab6f80f1d88573b73b882ab23168716
reyleal27 <reyvincentleal@gmail.com>
2024-05-16 21:27:58 +0800

    node hw 1

---
+++ index.js
@@ -0,0 +1,46 @@
+import { Command } from "commander";
+const program = new Command();
+
+import { listContacts, addContact,removeContact,getContactById } from "./contacts.js";
+
+
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone")
+    .allowUnknownOption(true);
+
+program.parse();
+
+const argv = program.opts();
+
+const invokeAction = async ({action, id, name, email, phone}) => {
+  switch (action) {
+    case "list":
+      const contacts= await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contactsById = await getContactById(id);
+      console.log(contactsById);
+      break;
+
+    case "add":
+      const addNewContact = await addContact ({name, email, phone});
+      console.log(addNewContact);
+      break;
+      
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
